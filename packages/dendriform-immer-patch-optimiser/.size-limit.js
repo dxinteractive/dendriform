dfab0bda86e988e22476bf7c11559962b61755af
Damien Clarke <dxinteractive@gmail.com>
2020-10-21 08:59:34 +1100

    feat: add library exports

---
+++ packages/dendriform-immer-patch-optimiser/.size-limit.js
@@ -0,0 +1,36 @@
+module.exports = [
+    {
+        name: 'everything combined',
+        path: "dist/dendriform-immer-patch-optimiser.esm.js",
+        limit: "1.1 KB",
+        ignore: ['immer']
+    },
+    {
+        name: 'traverse',
+        path: "dist/dendriform-immer-patch-optimiser.esm.js",
+        import: "{ traverse }",
+        limit: "0.2 KB",
+        ignore: ['immer']
+    },
+    {
+        name: 'zoomPatches',
+        path: "dist/dendriform-immer-patch-optimiser.esm.js",
+        import: "{ zoomPatches }",
+        limit: "0.2 KB",
+        ignore: ['immer']
+    },
+    {
+        name: 'applyPatches',
+        path: "dist/dendriform-immer-patch-optimiser.esm.js",
+        import: "{ applyPatches }",
+        limit: "0.4 KB",
+        ignore: ['immer']
+    },
+    {
+        name: 'optimise',
+        path: "dist/dendriform-immer-patch-optimiser.esm.js",
+        import: "{ optimise }",
+        limit: "0.8 KB",
+        ignore: ['immer']
+    }
+];
---
+++ packages/dendriform-immer-patch-optimiser/src/index.ts
@@ -0,0 +1,5 @@
+export * from './traverse';
+export * from './zoomPatches';
+export * from './optimise';
+export * from './applyPatches';
+export * from './types';
