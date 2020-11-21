5fa702e2d7a8d6876ea89fe31b6d2158cfde89f7
Damien Clarke <dxinteractive@gmail.com>
2020-11-21 20:19:16 +1100

    docs: rename docs > demo, update demo code

---
+++ packages/dendriform-demo/next.config.js
@@ -0,0 +1,12 @@
+// next.config.js
+// const withTM = require('@weco/next-plugin-transpile-modules');
+
+var path = require('path');
+
+module.exports = {
+    basePath: '/dendriform',
+    webpack: function (config) {
+        // config.resolve.alias['immer'] = path.resolve('../dendriform/node_modules/immer');
+        return config;
+    }
+};
