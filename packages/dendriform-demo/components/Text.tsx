ecc6a8f7b192366710e72cb0748bbd8ef7750915
Damien Clarke <dxinteractive@gmail.com>
2021-03-30 09:08:02 +1100

    wip: wip

--- packages/dendriform-demo/components/Text.tsx
+++ packages/dendriform-demo/components/Text.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import {space, textStyle, color, fontSize} from 'styled-system';
 
