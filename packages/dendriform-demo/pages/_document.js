47e046cc7cf57f878013db651a7fe11a5668664e
Damien Clarke <dxinteractive@gmail.com>
2021-03-30 10:06:50 +1100

    wip: wip

--- packages/dendriform-demo/pages/_document.js
+++ packages/dendriform-demo/pages/_document.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import Document from 'next/document';
+import Document, {Html, Head, Main, NextScript} from 'next/document';
 import {ServerStyleSheet} from 'styled-components';
 
 export default class MyDocument extends Document {
@@ -27,5 +27,22 @@ export default class MyDocument extends Document {
         } finally {
             sheet.seal();
         }
+    }
+
+    render() {
+        return <Html>
+            <Head>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
+                <meta name="msapplication-TileColor" content="#da532c" />
+                <meta name="theme-color" content="#ffffff" />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>;
     }
 }
