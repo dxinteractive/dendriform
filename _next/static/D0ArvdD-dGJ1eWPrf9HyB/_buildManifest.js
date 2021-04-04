44853e710569c1dc50cc1da52509ebe8b54cee2e
dxinteractive <dxinteractive@users.noreply.github.com>
2021-04-04 06:30:24 +0000

    deploy: 1ed546d682e4b4ec9733221a199de0fec79f8062

---
+++ _next/static/D0ArvdD-dGJ1eWPrf9HyB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002F1cc2734a.8b860ce4d6f14add8a6d.js","static\u002Fchunks\u002Fpages\u002Findex-8c24168e4a971f66a48b.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-2b72f391ff4c972e8758.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
