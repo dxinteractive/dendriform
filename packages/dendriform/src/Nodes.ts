da4fabd4685f910e8e59a10621a89374491f1270
Damien Clarke <dxinteractive@gmail.com>
2021-09-07 21:16:38 +1000

    feat: clean up nodes more accurately

--- packages/dendriform/src/Nodes.ts
+++ packages/dendriform/src/Nodes.ts
@@ -1,4 +1,4 @@
-import {BASIC, OBJECT, ARRAY, MAP, getType, get, set, each, create, applyPatches} from 'dendriform-immer-patch-optimiser';
+import {BASIC, OBJECT, ARRAY, MAP, getType, has, get, set, each, create, applyPatches} from 'dendriform-immer-patch-optimiser';
 import type {Path, DendriformPatch} from 'dendriform-immer-patch-optimiser';
 import {produceWithPatches} from 'immer';
 
@@ -140,7 +140,13 @@ export const updateNode = (nodes: Nodes, id: string, value: unknown): void => {
     if(type !== ARRAY && type === node.type) {
         if(type === BASIC) return;
         each(node.child, (childId, childKey) => {
-            updateNode(nodes, childId as string, get(value, childKey));
+            if(has(value, childKey)) {
+                updateNode(nodes, childId as string, get(value, childKey));
+            } else {
+                removeNode(nodes, childId as string);
+                // @ts-ignore
+                delete node.child[childKey];
+            }
         });
         return;
     }
