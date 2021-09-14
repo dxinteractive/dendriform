ad988918fca52a56c46965e0f2d2b67969491666
Damien Clarke <dxinteractive@gmail.com>
2021-09-15 08:38:08 +1000

    ci: dont measure small exports

--- packages/dendriform/.size-limit.js
+++ packages/dendriform/.size-limit.js
@@ -6,45 +6,10 @@ module.exports = [
         ignore: ['react', 'react-dom']
     },
     {
-        name: 'array',
-        path: "dist/dendriform.esm.js",
-        import: "{ array }",
-        limit: "0.4 KB",
-        ignore: ['react', 'react-dom', 'immer', 'shallow-equals']
-    },
-    {
         name: 'Dendriform',
         path: "dist/dendriform.esm.js",
         import: "{ Dendriform }",
         limit: "8.6 KB",
         ignore: ['react', 'react-dom']
-    },
-    {
-        name: 'useCheckbox',
-        path: "dist/dendriform.esm.js",
-        import: "{ useCheckbox }",
-        limit: "0.2 KB",
-        ignore: ['react', 'react-dom', 'immer', 'shallow-equals']
-    },
-    {
-        name: 'useInput',
-        path: "dist/dendriform.esm.js",
-        import: "{ useInput }",
-        limit: "0.3 KB",
-        ignore: ['react', 'react-dom', 'immer', 'shallow-equals']
-    },
-    {
-        name: 'sync',
-        path: "dist/dendriform.esm.js",
-        import: "{ sync }",
-        limit: "0.4 KB",
-        ignore: ['react', 'react-dom', 'immer', 'shallow-equals']
-    },
-    {
-        name: 'useSync',
-        path: "dist/dendriform.esm.js",
-        import: "{ useSync }",
-        limit: "0.4 KB",
-        ignore: ['react', 'react-dom', 'immer', 'shallow-equals']
     }
 ];
